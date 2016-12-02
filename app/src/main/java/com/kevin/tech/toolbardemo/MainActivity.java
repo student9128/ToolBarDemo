0c6516ef8b8f6c2b0d8a40d3c28d5fa96234ef3c
student9128 <student9128@gmail.com>
2016-12-02 15:14:59 +0800

    Initial commit

---
+++ app/src/main/java/com/kevin/tech/toolbardemo/MainActivity.java
@@ -0,0 +1,36 @@
+package com.kevin.tech.toolbardemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.widget.LinearLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tool_bar)
+    Toolbar mToolBar;
+    @BindView(R.id.activity_main)
+    LinearLayout mActivityMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mToolBar.setNavigationIcon(R.drawable.navigation);
+//        mToolBar.setTitleTextColor(ContextCompat.getColor(this,R.color.colorAccent));
+        setSupportActionBar(mToolBar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+}
